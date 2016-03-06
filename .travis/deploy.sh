911e5337495400c306fbf5e6a2bd368efe3f6661
udayakanth1122 <root@UDAYAKANTHs-Mac-mini.local>
2016-03-05 20:04:42 -0500

    resolved issue with travis deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,6 +3,6 @@
 # Helper script run from travis to deploy the app
 #
 # deploy to davonez in addition to ionic view for debug
-ionic upload --email $IONIC_EMAIL --password $IONIC_PASSWORD
+ionic upload --email challengepeace@gmail.com --password testapp2016
 ssh-keyscan 176.58.107.25  >> ~/.ssh/known_hosts
 scp -i .travis/travis_rsa.pem platforms/android/build/outputs/apk/android-debug.apk shazleto@176.58.107.25:~/clicker
