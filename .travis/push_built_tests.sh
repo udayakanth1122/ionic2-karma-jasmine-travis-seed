ce84782350d11116352d6fed021f2fbe971097fc
udayakanth1122 <udayakanth.sripada@gmail.com>
2016-03-07 10:52:53 -0500

    Update push_built_tests.sh

--- .travis/push_built_tests.sh
+++ .travis/push_built_tests.sh
@@ -12,7 +12,7 @@ ssh-add .travis/travis_rsa.pem
 # using -f as www/build is in .gitignore for dev purposes
 git add -f www/build/test/app
 git remote rm origin                                       # originally cloned by travis on https
-git remote add origin git@github.com:lathonez/clicker.git  # ditto
+git remote add origin git@github.com:udayakanth1122/ionic2-karma-jasmine-travis-seed-project.git  # ditto
 # careful not to trigger another build
 git -c user.name='travis' -c user.email='travis@travis-ci.org' commit -m "updating compiled tests [ci skip]"
 git push origin master
