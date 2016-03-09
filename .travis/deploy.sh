f1ea61ed7ec5ebcde7265180cee20544720e1e31
udayakanth1122 <root@UDAYAKANTHs-Mac-mini.local>
2016-03-09 00:36:35 -0500

    updated deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,6 @@
 #
 # deploy to davonez in addition to ionic view for debug
 ionic upload --email challengepeace@gmail.com --password testapp2016
+
 #ssh-keyscan 176.58.107.25  >> ~/.ssh/known_hosts
 #scp -i .travis/travis_rsa.pem platforms/android/build/outputs/apk/android-debug.apk shazleto@176.58.107.25:~/clicker
