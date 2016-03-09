320957ef70e40960d8793c7a6a256b09a8ee789a
udayakanth1122 <root@UDAYAKANTHs-Mac-mini.local>
2016-03-08 21:56:29 -0500

    added changes

--- .travis/push_built_tests.sh
+++ .travis/push_built_tests.sh
@@ -6,14 +6,14 @@
 git checkout master
 git checkout .
 # decrypt private key that gives us push access and add it to ssh agent
-# openssl aes-256-cbc -K $encrypted_a2f47a5da016_key -iv $encrypted_a2f47a5da016_iv -in .travis/travis_rsa.pem.enc -out .travis/travis_rsa.pem -d                      #
-# eval "$(ssh-agent -s)"
-# chmod 600 .travis/travis_rsa.pem
-# ssh-add .travis/travis_rsa.pem
+openssl aes-256-cbc -K $encrypted_a2f47a5da016_key -iv $encrypted_a2f47a5da016_iv -in .travis/travis_rsa.pem.enc -out .travis/travis_rsa.pem -d                      #
+eval "$(ssh-agent -s)"
+chmod 600 .travis/travis_rsa.pem
+ssh-add .travis/travis_rsa.pem
 # using -f as www/build is in .gitignore for dev purposes
-# git add -f www/build/test/app
-# git remote rm origin                                       # originally cloned by travis on https
-# git remote add origin git@github.com:udayakanth1122/ionic2-karma-jasmine-travis-seed-project.git  # ditto
+git add -f www/build/test/app
+git remote rm origin                                       # originally cloned by travis on https
+git remote add origin git@github.com:udayakanth1122/ionic2-karma-jasmine-travis-seed.git  # ditto
 # careful not to trigger another build
-# git -c user.name='travis' -c user.email='travis@travis-ci.org' commit -m "updating compiled tests [ci skip]"
-# git push origin master
+git -c user.name='travis' -c user.email='travis@travis-ci.org' commit -m "updating compiled tests [ci skip]"
+git push origin master
