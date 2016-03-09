debb281e4842ff05d3ec17bff165fd3d68d1c40a
udayakanth1122 <root@UDAYAKANTHs-Mac-mini.local>
2016-03-08 21:45:14 -0500

    adding ionic login

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,6 +3,7 @@
 # Helper script run from travis to deploy the app
 #
 # deploy to davonez in addition to ionic view for debug
+ionic login --email challengepeace@gmail.com --password testapp2016
 ionic upload --email challengepeace@gmail.com --password testapp2016
 #ssh-keyscan 176.58.107.25  >> ~/.ssh/known_hosts
 #scp -i .travis/travis_rsa.pem platforms/android/build/outputs/apk/android-debug.apk shazleto@176.58.107.25:~/clicker
